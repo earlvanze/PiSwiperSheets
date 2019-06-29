948d32232e10d7d28663c2e67597ea37b571d336
Earl Co <earlvanze@gmail.com>
2019-06-29 05:04:41 -0400

    this kind of works, need to play with it some more

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-from select import select
+#!/usr/bin/env python
+
+# MagTek MSR100 Mini Swipe Card Reader
+# Written By: Jeffrey Ness
+# 
+# Some Thanks need to go out to
+# http://www.micahcarrick.com/credit-card-reader-pyusb.html
+# for helping me get on the right track
+# With 16x2 LCD Support via Adafruit
+# https://learn.adafruit.com/drive-a-16x2-lcd-directly-with-a-raspberry-pi/
+
 from subprocess import Popen, PIPE
 from time import sleep
 from datetime import datetime
@@ -7,9 +17,148 @@ import digitalio
 import adafruit_character_lcd.character_lcd as characterlcd
 import sys
 import os
+import usb.core
+import usb.util
 import traceback
-import threading
-from main import *
+
+# MagTek Device MSR100 Mini Swipe
+vendorid = 0xc216
+productid = 0x0180
+
+# Define our Character Map per Reference Manual
+# http://www.magtek.com/documentation/public/99875206-17.01.pdf
+
+chrMap = {
+    4:  'a',
+    5:  'b',
+    6:  'c',
+    7:  'd',
+    8:  'e',
+    9:  'f',
+    10: 'g',
+    11: 'h',
+    12: 'i',
+    13: 'j',
+    14: 'k',
+    15: 'l',
+    16: 'm',
+    17: 'n',
+    18: 'o',
+    19: 'p',
+    20: 'q',
+    21: 'r',
+    22: 's',
+    23: 't',
+    24: 'u',
+    25: 'v',
+    26: 'w',
+    27: 'x',
+    28: 'y',
+    29: 'z',
+    30: '1',
+    31: '2',
+    32: '3',
+    33: '4',
+    34: '5',
+    35: '6',
+    36: '7',
+    37: '8',
+    38: '9',
+    39: '0',
+    40: '\n',
+    41: 'KEY_ESCAPE',
+    42: 'KEY_BACKSPACE',
+    43: '\t',
+    44: ' ',
+    45: '-',
+    46: '=',
+    47: '[',
+    48: ']',
+    49: '\\',
+    51: ';',
+    52: '\'',
+    53: '`',
+    54: ',',
+    55: '.',
+    56: '/',
+    57: 'KEY_CAPSLOCK'
+}
+
+shiftchrMap = {
+    4:  'A',
+    5:  'B',
+    6:  'C',
+    7:  'D',
+    8:  'E',
+    9:  'F',
+    10: 'G',
+    11: 'H',
+    12: 'I',
+    13: 'J',
+    14: 'K',
+    15: 'L',
+    16: 'M',
+    17: 'N',
+    18: 'O',
+    19: 'P',
+    20: 'Q',
+    21: 'R',
+    22: 'S',
+    23: 'T',
+    24: 'U',
+    25: 'V',
+    26: 'W',
+    27: 'X',
+    28: 'Y',
+    29: 'Z',
+    30: '!',
+    31: '@',
+    32: '#',
+    33: '$',
+    34: '%',
+    35: '^',
+    36: '&',
+    37: '*',
+    38: '(',
+    39: ')',
+    40: '\n',
+    41: 'KEY_ESCAPE',
+    42: 'KEY_BACKSPACE',
+    43: '\t',
+    44: ' ',
+    45: '_',
+    46: '+',
+    47: '{',
+    48: '}',
+    49: '|',
+    51: ':',
+    52: '"',
+    53: '~',
+    54: '<',
+    55: '>',
+    56: '?',
+    57: 'KEY_CAPSLOCK'
+}
+
+# find our device by id
+device = usb.core.find(idVendor=vendorid, idProduct=productid)
+if device is None:
+    raise Exception('Could not find USB Card Reader')
+
+# remove device from kernel, this should stop
+# reader from printing to screen and remove /dev/input
+if device.is_kernel_driver_active(0):
+    try:
+        device.detach_kernel_driver(0)
+    except usb.core.USBError as e:
+        raise Exception("Could not detach kernel driver: %s" % str(e))
+
+# load our devices configuration
+try:
+    device.set_configuration()
+    device.reset()
+except usb.core.USBError as e:
+    raise Exception("Could not set configuration: %s" % str(e))
 
 # Modify this if you have a different sized character LCD
 lcd_columns = 16
@@ -62,34 +211,76 @@ lcd.clear()
 sleep(2)
 interface = find_interface()
 ip_address = parse_ip()
-data = ''
 
-def main(data):
+def main():
+    # get device endpoint information
+    endpoint = device[0][(0,0)][0]
+
+    swiped = False
+    data = []
+    datalist = []
+    print("Swipe card:")
+    
     while True:
+     
         # date and time
         lcd_line_1 = datetime.now().strftime('%m/%d %H:%M:%S\n')
         
         # current ip address
         lcd_line_2 = ip_address
 
-        # card was swiped
-        if data:
-            lcd.clear()
+        # read card swiper
+        try:
+            results = device.read(endpoint.bEndpointAddress, endpoint.wMaxPacketSize, timeout=5)
+            if results:
+                data += results
+                datalist.append(results)
+                swiped = True
+
+        except usb.core.USBError as e:
+            if e.args[1] == 'Operation timed out' and swiped:
+                print('Swiped:', swiped)
+                swiped = False
+
+        # create a list of 8 bit bytes and remove empty bytes
+        ndata = []
+        for d in datalist:
+            if d.tolist() != [0, 0, 0, 0, 0, 0, 0, 0]:
+                ndata.append(d.tolist())
+
+        # parse over our bytes and create string to final return
+        sdata = ''
+        for n in ndata:
+            #print(n[0], n[2], chrMap[n[2]], shiftchrMap[n[2]])
+            # handle non shifted letters
+            if n[2] in chrMap and n[0] == 0:
+                sdata += chrMap[n[2]]
+                
+            # handle shifted letters
+            if n[2] in shiftchrMap and n[0] != 0:
+                sdata += shiftchrMap[n[2]]
+
+        lcd.clear()
+        if sdata:
             try:
-                data = ''.join(data).split('^')
-                print(data)
-                card_num = data[0].strip('%B')
-                last_name, first_name = data[1].strip().split('/')
+                sdata = ''.join(sdata).split('^')
+                print(sdata)
+                card_num = sdata[0].strip('%B')
+                last_name, first_name = sdata[1].strip().split('/')
                 print(first_name, last_name, card_num)
                 lcd_line_2 = "Hi, {0}!".format(first_name.title())
-            except Exception as e:
+            except Exception as err:
                 traceback.print_exc()
-                lcd_line_2 = str(e.args[0])
-            finally:
-                lcd.message = lcd_line_1 + lcd_line_2
-                sleep(2)
-                lcd.clear()
-                lcd_line_2 = "Swipe card..."
+                lcd_line_2 = str(err.args[0])
+                
+            sdata = ''
+            lcd.message = lcd_line_1 + lcd_line_2
+            sleep(2)
+            lcd.clear()
+            lcd_line_2 = "Swipe card..."
+            data = []
+            datalist = []
+        
         try:
             # combine both lines into one update to the display
             lcd.message = lcd_line_1 + lcd_line_2
@@ -99,8 +290,5 @@ def main(data):
         
         sleep(1)
 
-thread = threading.Thread(target=read_card(data), args=(data))
-thread.start()
-
 if __name__ == '__main__':
-    main(data)
+    main()
